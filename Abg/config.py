9cf0a8545ee530bf387111084844e6a64ae1a407
Tobixy <shivamrajput69bs@gmail.com>
2024-10-01 13:46:50 +0530

    Update config.py

--- Abg/config.py
+++ Abg/config.py
@@ -1,11 +1,14 @@
 from os import getenv
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config(object):
+    OWNER_ID = int(getenv("OWNER_ID", "5715764478"))
+    LOGGER_ID = getenv("LOGGER_ID", "-1001819078701")
+    TIME_ZONE = getenv("TIME_ZONE", "Asia/Kolkata")
+    DEV_USERS = "5715764478"
     HANDLER = getenv("HANDLER", "/ ! + . $ #").split()
-    devs_env = getenv("DEVS")
-    DEVS = list(map(int, devs_env.split())) if devs_env else []
+
+DEV_USER = Config.DEV_USERS
+DEVS = list(set([int(Config.OWNER_ID)] + [int(DEV_USER)]))
